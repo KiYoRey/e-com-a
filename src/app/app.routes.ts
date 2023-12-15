fa820461a030e57fdf12e7407a87c0c6afeb13bc
widad <widadtalhaoui2001@gmail.com>
2023-12-15 01:29:29 +0100

    adding product ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { ProductsDetailsComponent } from './products/components/products-details/products-details.component';
+import { CartComponent } from './carts/components/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"products", component:AllProductsComponent},
+    {path:"det", component:ProductsDetailsComponent},
+    {path:"carts", component:CartComponent},
+    {path:"**", redirectTo:"products", pathMatch:'full'}
+
+
+
+];
