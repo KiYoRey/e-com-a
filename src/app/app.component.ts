7c3d4a2f011f63beb8bccb2b55f9318c66a32034
widad <widadtalhaoui2001@gmail.com>
2023-12-15 15:08:08 +0100

    gettin the products from an api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './shared/components/header/header.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SharedModule, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, SharedModule, HeaderComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-all-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './all-products.component.html',
   styleUrl: './all-products.component.scss'
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  
+  item: any =[];
+
+  ngOnInit(): void {
+    this.getALLProducts();
+  }
+  getALLProducts(){
+    this.httpClient.get('https://fakestoreapi.com/products').subscribe((item: any) =>{
+      console.log(item);
+      this.item = item;
+    })
+  }
 
 }
